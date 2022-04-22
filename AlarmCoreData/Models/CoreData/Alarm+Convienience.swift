798843a9eb719306595e71520cbf8914d87c53f9
TasukuY <alohaty81@gmail.com>
2022-04-21 22:16:11 -0600

    created the models

---
+++ AlarmCoreData/Models/CoreData/Alarm+Convienience.swift
@@ -0,0 +1,21 @@
+//
+//  Alarm+Convienience.swift
+//  AlarmCoreData
+//
+//  Created by Tasuku Yamamoto on 4/21/22.
+//
+
+import Foundation
+import CoreData
+
+extension Alarm {
+    @discardableResult convenience init(title: String, fireDate: Date,
+                                        isEnabled: Bool,
+                                        context: NSManagedObjectContext = CoreDataStack.context){
+        self.init(context: context)
+        self.uuidString = UUID().uuidString
+        self.title = title
+        self.fireDate = fireDate
+        self.isEnabled = isEnabled
+    }
+}
---
+++ AlarmCoreData/Strings/Strings.swift
@@ -0,0 +1,13 @@
+//
+//  Strings.swift
+//  AlarmCoreData
+//
+//  Created by Tasuku Yamamoto on 4/21/22.
+//
+
+import Foundation
+
+enum Strings {
+    
+}
+
